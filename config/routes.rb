4d4abb0fdc6fee50f983a2e6d22b853fbfb78e61
Ronan Girault <girault.ronan@gmail.com>
2017-11-21 16:31:00 +0100

    Add links to articles views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  #resources :articles
+
+  get 'articles',          to: 'articles#index'
+
+  get 'articles/new',      to: 'articles#new'
+  post 'articles',         to: 'articles#create'
+
+  get 'articles/:id',      to: 'articles#show', as: 'article'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: 'articles_edit'
+  patch 'articles/:id',    to: 'articles#update'
+
+  delete 'articles/:id',   to: 'articles#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
